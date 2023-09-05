a64d3d12283c3a52035489bc4b12bc1b935fa07f
linx <1824517828@qq.com>
2023-09-05 14:08:28 +0800

    iota的用法

--- basic/iota的用法/main.go
+++ basic/iota的用法/main.go
@@ -19,6 +19,16 @@ const (
 	Status7              //106
 )
 
+const (
+	status1 = iota       //  0 注意iota出现在第几行它的值就是几
+	status2 = iota - 1   //  0
+	status3 = iota - 1   //  1
+	status4 = iota + 100 //  103
+	status5 = iota - 1   //  3
+	status6 = iota - 1   //  4
+	status7 = iota - 1   //  5
+)
+
 type ObjectType int
 
 // 下面两种用法其实是一样的
